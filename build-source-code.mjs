8f7434566f2df034ae2efdcc12486c86188b68db
hayden194 <104180881+hayden194@users.noreply.github.com>
2022-07-19 00:00:15 +0800

    Update build-source-code.mjs

--- build-source-code.mjs
+++ build-source-code.mjs
@@ -1,1 +1,5 @@
-
+cd ${{ steps.env.outputs.USER_WORK_SPACE_PATH }}
+#modify maven package version
+mvn versions:set -DnewVersion=${{ github.event.inputs.JAR_VERSION }} -s settings.xml
+#maven package
+mvn clean package -U -s settings.xml
