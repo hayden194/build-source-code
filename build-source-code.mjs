ddf5f59d020725e6c8e067a9911c593df56fd5f8
hayden194 <104180881+hayden194@users.noreply.github.com>
2022-07-19 22:13:39 +0800

    Update build-source-code.mjs

--- build-source-code.mjs
+++ build-source-code.mjs
@@ -2,6 +2,7 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const zx =require('zx/globals');
 
 try {
   const workspace=github.context.github_workspace;
@@ -9,14 +10,14 @@ try {
   const user_work_space=workspace+"/"+github_account;
   console.log(user_work_space);
   
-  cd (user_work_space);
+  zx cd (user_work_space);
   //modify maven package version
   const jar_version=core.getInput('JAR_VERSION');
   console.log(jar_version);
-  echo (jar_version);
-  mvn versions:set -DnewVersion=jar_version;
+  zx echo (jar_version);
+  zx mvn versions:set -DnewVersion=jar_version;
   //maven package
-  mvn clean package -U
+  zx mvn clean package -U
 } catch (error) {
   core.setFailed(error.message);
 }
