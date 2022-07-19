21ebc96c5a401f9c6a824bca9dd3237e8ff5b3a2
hayden194 <104180881+hayden194@users.noreply.github.com>
2022-07-20 00:00:43 +0800

    Update build-source-code.mjs

--- build-source-code.mjs
+++ build-source-code.mjs
@@ -1,21 +1,27 @@
+#!/usr/bin/env zx
+
 const core = require('@actions/core');
 const github = require('@actions/github');
-const zx =require('zx/globals');
 
 try {
-  const workspace=github.context.github_workspace;
-  const github_account=github.context.github_actor;
+  const workspace=core.getInput('WORKSPACE');
+  const github_account=github.context.actor;
   const user_work_space=workspace+"/"+github_account;
-  console.log(user_work_space);
+  console.log(user_work_space+"*******");
   
-  zx cd (user_work_space);
-  //modify maven package version
   const jar_version=core.getInput('JAR_VERSION');
-  console.log(jar_version);
-  zx echo (jar_version);
-  zx mvn versions:set -DnewVersion=jar_version;
-  //maven package
-  zx mvn clean package -U
+  console.log(jar_version+"&&&&&&&&&&&");
+  
+  echo('invoke zx success');
+  cd (user_work_space);
+  pwd
+//   //modify maven package version
+//   const jar_version=core.getInput('JAR_VERSION');
+//   console.log(jar_version);
+//   echo (jar_version);
+//   zx mvn versions:set -DnewVersion=jar_version;
+//   //maven package
+//   mvn clean package -U
 } catch (error) {
   core.setFailed(error.message);
 }
