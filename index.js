131be19bf8e3f800a07234bc8fe279af596e02e2
hayden194 <104180881+hayden194@users.noreply.github.com>
2022-07-19 22:42:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+// import 'zx/globals'
+
 const core = require('@actions/core');
 const github = require('@actions/github');
-const zx =require('zx/globals');
 
 try {
   const workspace=github.context.github_workspace;
   const github_account=github.context.github_actor;
   const user_work_space=workspace+"/"+github_account;
-  console.log(user_work_space);
+  console.log(user_work_space+"*******");
   
 //   cd (user_work_space);
 //   //modify maven package version
